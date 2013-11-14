a21882255a2afdd623105bcbb9578a8fc619ea22
munozhoracio@gmail.com <>
2013-11-14 21:25:18 +0000

    sdasda

--- src/main/java/adapter/Client.java
+++ src/main/java/adapter/Client.java
@@ -39,6 +39,28 @@ public class Client
 	public void doWork() {
 		// TODO : to implement	
 	}
+ 
+ 
+ public String version;
+
+/**
+ * @return the version
+ */
+public String getVersion()
+{
+   return version;
+}
+
+/**
+ * @param version the version to set
+ */
+public void setVersion(String version)
+{
+   this.version = version;
+}
+ 
+ 
+ 
 	
 }
 
